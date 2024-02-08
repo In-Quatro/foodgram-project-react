45f605ed4c0e06e9d63108d15bd9b0da0ab18d3f
Алексей Шубенков <shubenkov-93@yandex.ru>
2024-02-09 00:44:45 +0300

    Готовый API, PEP8

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,15 +5,11 @@ class IsAuthorPermission(permissions.BasePermission):
     """Изменение объектов доступно только для Автора и Администратора."""
 
     def has_permission(self, request, view):
-        return (
-                request.user.is_authenticated
-                or request.method in permissions.SAFE_METHODS
-        )
+        return (request.user.is_authenticated
+                or request.method in permissions.SAFE_METHODS)
 
     def has_object_permission(self, request, view, obj):
-        return (
-            obj.author == request.user
-            or request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.is_superuser
-        )
+        return (obj.author == request.user
+                or request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+                and request.user.is_superuser)
